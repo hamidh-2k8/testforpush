581208d77e6d097ddafe52045b3dc5fde1e6db98
hamidh-2k8 <hamidh.2k8@gmail.com>
2023-09-30 01:24:53 -0400

    i have made changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ihavemadechanges.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+public class ihavemadechanges {
+    
+    private boolean haveIMadeChanges = true;
+
+    public boolean getHaveIMadeChanges() {
+
+        return haveIMadeChanges;
+        
+    }
+
+}
